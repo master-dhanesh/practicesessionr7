4f75c0d6ee3ce08c8f93edcec63618b4c2686220
master-dhanesh <dhanesh1296@gmail.com>
2022-08-19 21:10:43 +0530

    Gallery 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const GetImages = async () => {
+        try {
+            const { data } = await axios.get("https://picsum.photos/v2/list");
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div>
+            <button onClick={GetImages}>Get Images</button>
+        </div>
+    );
+};
 
 export default App;
